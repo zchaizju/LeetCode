7ec18478c2c400e4a3bf1decb4c2cee9996f0868
Chai Zheng <zchaizju@gmail.com>
2018-08-21 16:22:57 +0800

    Create Solution.py

---
+++ 13. Roman to Integer/Solution.py
@@ -0,0 +1,68 @@
+"""
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+
+Example 1:
+
+Input: "III"
+Output: 3
+Example 2:
+
+Input: "IV"
+Output: 4
+Example 3:
+
+Input: "IX"
+Output: 9
+Example 4:
+
+Input: "LVIII"
+Output: 58
+Explanation: C = 100, L = 50, XXX = 30 and III = 3.
+Example 5:
+
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+"""
+
+"""
+以MCM为例，若s[i+1]的键值比s[i]大，则这两位为一组。即，M为第一个数，CM为第二个数。相加为1000+1000-100=1900。
+OJ Runtime：136 ms. TOP 35%.
+"""
+
+class Solution:
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dict = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        sum = 0
+        i = 0
+        while i < len(s)-1:
+            if dict[s[i+1]] > dict[s[i]]:
+                sum += dict[s[i+1]]-dict[s[i]]
+                i += 2
+            else:
+                sum += dict[s[i]]
+                i += 1
+        if i == len(s)-1:
+            sum += dict[s[i]]
+        return sum
